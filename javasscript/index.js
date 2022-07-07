5fd84a20e5a82be38c129685a334cc92ab066362
Southen_ <alabanaomicharlesduke@gmail.com>
2022-07-07 15:33:35 +0100

    first commit

---
+++ javasscript/index.js
@@ -0,0 +1,300 @@
+// 
+
+// var1 = 16; var2 = 40;
+
+// console.log("Helllooooo")
+
+
+
+// function sayHello() {
+//     alert("Erro")  
+// }
+
+
+// const myVar = "global";
+
+// function checkscope(){
+//     var myVar = "local";
+//     console.log(myVar)
+
+// }
+
+// var a = 50;
+// var b = 15;
+// var c = "Test";
+// var linebreak = "<br/>;"
+
+// document.write("a + b = ")
+// result = a + b;
+// document.write(result);
+// document.write(linebreak)
+
+
+// document.write("a - b = ")
+// result = a - b;
+// document.write(result)
+// document.write(linebreak)
+
+
+// document.write(" a / b =")
+// result = a / b ;
+// document.write(result)
+// document.write(linebreak)
+
+
+
+// document.write(" a % b = ");
+// result = a % b ;
+// document.write(result)
+// document.write(linebreak)
+
+
+// document.write("a + b + c =")
+// result = a + b + c;
+// document.write(result)
+// document.write(linebreak)
+
+
+// a =a++
+// document.write("a++ =")
+// result = a++;
+// document.write(result)
+// document.write(linebreak)
+
+
+// b = b++
+// document.write("b-- =")
+// result = b--;
+// document.write(result)
+// document.write(linebreak)
+
+
+// var c = 20;
+// var d = 15 ;
+// var linebreak = " <br/>";
+
+
+// document.write("(c == d) =>");
+// result = (c == d)
+// document.write( result)
+// document.write(linebreak)
+
+
+// document.write("(c < d)=>")
+// result = (c < d )
+// document.write(result)
+// document.write(linebreak)
+
+
+// document.write("(c > d)=>")
+// result = (c > d )
+// document.write(result)
+// document.write(linebreak)
+
+
+// document.write("(c != d)=>")
+// result = (c != d);
+// document.write(result);
+// document.write(linebreak)
+
+
+// document.write("(c >= d)=>")
+// result = (c >= d);
+// document.write(result);
+// document.write(linebreak)
+
+
+// document.write("(c <= d)=>")
+// result = (c <= d );
+// document.write(result);
+// document.write(linebreak)
+
+
+// var e = true;
+// var f = false;
+// var linebreak = "<br/>"
+
+
+// document.write("(e && f) =>")
+// result = (e && f )
+// document.write(result);
+// document.write(linebreak)
+
+
+// document.write("(e || f) =>");
+// result = (e || f)
+// document.write(result);
+// document.write(linebreak)
+
+
+// document.write("!(a && b) =>");
+// result = (!(e && f))
+// document.write(result);
+// document.write(linebreak)
+
+
+
+
+
+// **  question 1   *    //
+
+// const Allusers = Users.map((User)=>{
+//     return{
+//         id:User.id,
+//         firsrName:User.first_name,
+//         lastname:User.last_name,
+//         email:User.email,
+//         age:User.age,
+//         isActive:User.isActive,
+//         gender:User.gender
+//     }
+// })
+//    console.log(Allusers);
+   
+// **  question 2       *  //
+
+// const Userfilter = Users.filter((User) =>User.isActive === false)
+   
+// console.log(Userfilter);
+
+//  **  queston 3  *//
+//  const Userage = Users.filter((User) => {
+// if(User.age > 18 && User.age < 45 )
+// return(
+//     User.age
+// )
+// })
+
+// console.log(Userage);
+
+// **  question 4*//
+
+// const Userage = Users.filter((User) => {
+//     if(User.age > 0 && User.age < 10) 
+//     return(
+//         User.age
+//     )
+// })
+
+// console.log(Userage);
+
+// ** question 5 *//
+
+// const Usergender = Users.filter((User) => User.gender = "male")
+
+// console.log(Usergender);
+
+
+// const bio = (
+//         	{
+// 		id: 1,
+// 		first_name: "Trista",
+// 		last_name: "Rozzier",
+// 		email: "trozzier0@amazon.co.uk",
+// 		gender: "Female",
+// 		isActive: false,
+// 		age: 4,
+// 	}
+
+//   )
+//    console.log(bio.gender,...bio);
+
+//    const isMarried = 1
+
+//    console.log(Boolean(isMarried));
+
+
+//    function sum(...theArgs){
+// let total = 0;
+// for (const arg of theArgs){
+//     total += arg;
+// }
+// return total;
+//    }
+
+//    console.log(sum(1, 2, 3));
+
+//    console.log(sum(1, 2, 3, 4, 7, 10));
+
+
+
+//    let txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+//    let lenght = txt.lenght;
+// console.log(txt.slice(3, 20));
+
+
+
+// let text = "5";
+// let padded = ext.padstart(4, "0");
+// console.log(text);
+
+// ** string method * >..
+
+
+// let str = "Apple, Banana, Kiwi";
+// let part = str.substr;
+// console.log(str.substr(-7 -6));
+
+// let step = "Apple, Banana, Kiwi";
+// let par = step.slice;
+// console.log(step.slice(1));
+
+// let text ="Please visit Microsoft!";
+// let newText = text.replace
+// console.log(text.replace("Microsoft", "your mama house"));
+
+// let tex ="Please visit Microsoft!";
+// let newTex = tex.toLocaleUpperCase;
+// console.log(tex.toLocaleUpperCase());
+
+// let value = "go home amigo"
+// let newValuev = value.toUpperCase;
+// console.log(value.toUpperCase());
+
+
+
+const txt = "Charles Duke Alaba Naomi";
+const card = "5555-4334-321"; 
+const num = "250";
+// let result = txt.toUpperCase();
+
+result = "x".repeat(card.length);
+result = card.split("-")[2]
+// console.log(`${"x".repeat([card.length-4])} ${card.split("-")[2]}`);
+
+result = txt.toLowerCase();
+result = txt.slice();
+result = txt.startsWith();
+result = txt.endsWith();
+result = txt.substring();
+result = txt.replace();
+result = txt.replaceAll();
+result = txt.trim();
+result = txt.trim().length;
+result = txt.charAt(2).toUpperCase();
+result = txt[2].toUpperCase();
+result = txt.charCodeAt(5);
+result = txt.valueOf();
+result = txt.split(" ").join("/");
+result = "x".repeat(txt.lenght);
+// console.log("x".repeat(txt.length));
+result = num.padEnd()
+console.log(num.padEnd(10,"EA0"));
+result = txt.num.padStart(5,"EA");
+result = txt.includes(li);
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(txt.slice());
+
+   
